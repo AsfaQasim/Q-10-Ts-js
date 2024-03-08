d12fa77c36042574b4e56e09fbfe7017b1fdaaf8
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:31:11 -0800

    Q 10 of Ts/js & node js of 45 assignment
    Q 10 of Ts/js & node js of 45 assignment

---
+++ main.ts
@@ -0,0 +1,5 @@
+//exercise 10
+//my name is ASIFA
+//Dated: 25/2/2024
+//this program will run simple code just like hello world
+console.log(`hello world simple program`);
---
+++ program2.js
@@ -0,0 +1,4 @@
+//exercise 10
+//my name is ASIFA
+//Dated: 25/2/2024
+console.log(5 * 2);
